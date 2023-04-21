19a833a053416d6f5d0a96873b738919470761a6
Margaret Amanfu <margaretfafa@gmail.com>
2023-04-21 00:14:42 -0700

    typed and untyped constants

---
+++ constants/constants.go
@@ -0,0 +1,21 @@
+Typed Constants
+
+package main
+import ("fmt")
+
+const A int = 1
+
+func main() {
+  fmt.Println(A)
+}
+
+Untyped Constants
+
+package main
+import ("fmt")
+
+const A = 1
+
+func main() {
+  fmt.Println(A)
+}
