5cbbbfac778f367e6a246d8b8fdfe7cc5aa8e88c
Margaret Amanfu <margaretfafa@gmail.com>
2023-04-21 21:10:25 -0700

    arrays

---
+++ arrrays/arrays.go
@@ -0,0 +1,26 @@
+Two arrays (arr1 and arr2) with defined lengths:
+
+package main
+import ("fmt")
+
+func main() {
+  var arr1 = [3]int{1,2,3}
+  arr2 := [5]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+}
+
+
+Two arrays (arr1 and arr2) with inferred lengths
+
+package main
+import ("fmt")
+
+func main() {
+  var arr1 = [...]int{1,2,3}
+  arr2 := [...]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+}
