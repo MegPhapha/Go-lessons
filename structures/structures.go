359bd983f354cac942a2d03abb2c1858ae541147
Margaret Amanfu <margaretfafa@gmail.com>
2023-04-26 21:35:12 -0700

    structures

---
+++ structures/structures.go
@@ -0,0 +1,18 @@
+Syntax
+
+type struct_name struct {
+	member1 datatype;
+	member2 datatype;
+	member3 datatype;
+	...
+  }
+
+
+  // this example declares a struct type Person with the following members: name , age , job and salary:
+
+  type Person struct {
+	name string
+	age int
+	job string
+	salary int
+  }
